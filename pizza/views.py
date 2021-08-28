57e7d9de824f68b9485906c3a94313a5cee85e37
polinamoore <p_gerasimenko@yahoo.com>
2021-08-28 22:44:49 +0100

    first commit

---
+++ pizza/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .forms import PizzaForm
+from .models import Size, Pizza
+
+# Create your views here.
+def home(request):
+    return render(request, 'pizza/home.html')
+ 
+def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST) # instantiate PizzaForm object with filled data!
+        if filled_form.is_valid():
+            created_pizza = filled_form.save() # saving pizza in DB
+            created_pizza_pk = created_pizza.id # access ID of the order
+            note = f"Thanks for your order. Your {filled_form.cleaned_data['size']} {filled_form.cleaned_data['topping1']} and {filled_form.cleaned_data['topping2']} pizza is on its way!"
+            new_form = PizzaForm()
+        else:
+            created_pizza_pk = None
+            note = 'Pizza order has failed. Try again.'
+        context = {'created_pizza_pk': created_pizza_pk, 'pizzaform': filled_form, 'note': note}
+        return render(request, 'pizza/order.html', context)
+    else:
+        form = PizzaForm()
+        context = {'pizzaform': form}
+        return render(request, 'pizza/order.html', context)
+
+
+def edit_order(request, pk):
+    pizza = Pizza.objects.get(pk=pk) # Accessing this order by its PK
+    form = PizzaForm(instance=pizza) # Accessing that same form to edit it
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST, instance=pizza)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+    context = {'pizzaform': form, 'pizza': pizza}
+    return render(request, 'pizza/edit_order.html', context)
---
+++ pizzaheaven/urls.py
@@ -0,0 +1,27 @@
+"""pizzaheaven URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin # default, provided
+from django.urls import path
+from pizza import views  # views from our local app ("pizza", for ordering pizza). Different views will be addressed as "object.attribute" scheme (e.g. "views.order")
+
+urlpatterns = [
+    path('admin/', admin.site.urls), # default, provided by Django
+    path('', views.home, name='home'), # home route (e.g. "http://localhost:8000/")
+    path('order/', views.order, name='order'), # order pizza route ("http://localhost:8000/order")
+
+    path('order/<int:pk>/', views.edit_order, name='edit_order') # UPDATE (edit) order in db
+    # path('pizzas/', views.pizzas, name='pizzas')
+]
